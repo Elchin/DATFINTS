285a155884ade870fda8c875c3657657b468baf1
elchin <elchin@pn2002136.lanl.gov>
2021-06-01 15:44:16 -0600

    added CandleStick plot

--- datfints_utils.py
+++ datfints_utils.py
@@ -5,6 +5,10 @@ import datfints_utils as df_utils
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+# All functions described below can be found on this channel
+# https://www.youtube.com/channel/UCbmb5IoBtHZTpYZCDBOC1CA
 
 def calc_rsi(stock_index,date='2021-1-1'):
     '''
@@ -89,3 +93,105 @@ def stock_price_prediction(stock_index,date='2021-1-1'):
     
     
     return LinReg_prediction
+
+def on_balance_volume(stock_index,date='2021-1-1'):
+    '''
+    On Balance Volume shows when to buy or sell the stock 
+    Inputs:
+        stock_index: e.g. 'ETH-USD'
+        date:        e.g. '2021-1-1'
+    '''
+
+    stock=wb.DataReader(stock_index,start=date,data_source='yahoo') #stock ticker index
+    
+    #Calculate on Balance Volume
+    OBV=[]
+    OBV.append(0)
+    for i in range(1,len(stock.Close)):
+        if stock.Close[i] > stock.Close[i-1]:
+            OBV.append(OBV[-1]+stock.Volume[i])
+        elif stock.Close[i] < stock.Close[i-1]:
+            OBV.append(OBV[-1]-stock.Volume[i])
+        else:
+            OBV.append(OBV[-1])
+
+    # EMA exponential moving average
+    stock['OBV']=OBV
+    stock['OBV_EMA']=stock['OBV'].ewm(span=20).mean()
+    stock
+
+    plt.figure(figsize=(12.2,4.5))
+    plt.plot(stock.index,stock['OBV'],label='OBV')
+    plt.plot(stock.index,stock['OBV_EMA'],label='OBV_EMA')
+
+    x=buy_sell(stock,'OBV','OBV_EMA')
+    stock['Buy_Signal_Price']=x[0]
+    stock['Sell_Signal_Price']=x[1]
+
+    plt.figure(figsize=(12.2,4.5))
+    plt.plot(stock.index,stock['Close'],label='Close',alpha=0.35)
+    plt.scatter(stock.index,stock['Buy_Signal_Price'],label='Buy',marker='^',alpha=1,color='green')
+    plt.scatter(stock.index,stock['Sell_Signal_Price'],label='Sell',marker='v',alpha=1,color='red')
+    plt.legend()
+    plt.ylabel('Price [$]')
+    plt.xlabel('Date [d]')
+
+    return 
+
+#if OBV > OBV_EMA -> buy
+#if OBV < OBV_EMA -> buy
+def buy_sell(data,col1,col2):
+    '''
+    col1: OBV 
+    col2: OBV-EMA
+    '''
+    SigPriceBuy=[]
+    SigPriceSell=[]
+    flag=1
+    for i in range(0,len(data)):
+        if data[col1][i] > data[col2][i] and flag != 1:
+            SigPriceBuy.append(data['Close'][i])
+            SigPriceSell.append(np.nan)
+            flag=1
+        elif data[col1][i] > data[col2][i] and flag != 0:
+            SigPriceSell.append(data['Close'][i])
+            SigPriceBuy.append(np.nan)
+            flag=0
+        else:
+            SigPriceBuy.append(np.nan)
+            SigPriceSell.append(np.nan)
+            
+    return (SigPriceBuy,SigPriceSell)
+
+def plot_candlesticks(stock_index,date='2021-1-1'):
+    '''
+    Plot Japanese Candle Sticks in the interactive mode 
+    Make sure that you have plotly package installed
+    e.g. 'pip install plotly'
+    Inputs:
+        stock_index: e.g. 'ETH-USD'
+        date:        e.g. '2021-1-1'
+    '''
+    stock=wb.DataReader(stock_index,start=date,data_source='yahoo') #stock ticker index
+    #https://www.youtube.com/watch?v=4fhBXFSS1lc
+    figure = go.Figure(
+        data = [
+            go.Candlestick(
+                x = stock.index,
+                low = stock['Low'],
+                high = stock['High'],
+                close = stock['Close'],
+                open = stock['Open'],
+                increasing_line_color = 'green',
+                decreasing_line_color = 'red'
+            )
+        ]
+    )
+
+    figure.update_layout(
+        title=stock_index,
+        yaxis_title='Price ($)',
+        xaxis_title='Time'
+    )
+
+    figure.show()
